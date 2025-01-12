51d84eaf8479e17e886cbd0ae496761d6a3a771b
Monica Alves Pereira <92680257+MonicaAlvesP@users.noreply.github.com>
2025-01-12 21:35:57 +0000

    adiciona `clipboard` para copiar a senha

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import clipboardCopy from 'clipboard-copy';
 import './style.css';
 
 const passwordButton = document.querySelector('button');
@@ -8,3 +9,14 @@ passwordButton.addEventListener('click', () => {
   const password = nanoid(8);
   showPassword.innerHTML = password;
 });
+
+showPassword.addEventListener('click', () => {
+  const password = showPassword.innerHTML;
+  clipboardCopy(password)
+  .then(() => {
+    alert('Senha copiada para o clipboard!')
+  })
+  .catch((err) => {
+    console.log('Erro ao copiar para o clipboard:', err)
+  })
+})
